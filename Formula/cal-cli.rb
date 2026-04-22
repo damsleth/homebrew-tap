c5089797df6f9158e61c8a6525180d5aa868b082
Carl Joakim Damsleth <kim@damsleth.no>
2026-04-22 13:12:52 +0200

    cal-cli 0.4.0

--- Formula/cal-cli.rb
+++ Formula/cal-cli.rb
@@ -3,8 +3,8 @@ class CalCli < Formula
 
   desc "Calendar CLI for Outlook / Microsoft 365"
   homepage "https://github.com/damsleth/cal-cli"
-  url "https://github.com/damsleth/cal-cli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "43207d088b803b51113b0dffda8315db5d77d4f683eb949ed53dc7ab43ab585e"
+  url "https://github.com/damsleth/cal-cli/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "8c67d4be66b0468d5ec237f4e74a80cb25a0d0471eb4233e81e3775c5728fdbe"
   license "WTFPL"
   head "https://github.com/damsleth/cal-cli.git", branch: "main"
 

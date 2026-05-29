6a9d088dd818c770f26c528d267acc705880fa91
Carl Joakim Damsleth <kim@damsleth.no>
2026-05-29 15:06:42 +0200

    owa-tools 0.5.0

--- Formula/owa-tools.rb
+++ Formula/owa-tools.rb
@@ -1,11 +1,11 @@
 class OwaTools < Formula
   include Language::Python::Virtualenv
 
-  desc "Outlook/Microsoft 365 CLIs (cal, mail, graph, doctor, people, sched, drive)"
+  desc "Outlook/Microsoft 365 CLI suite (mail, calendar, graph, drive, todo)"
   homepage "https://github.com/damsleth/owa-tools"
-  url "https://github.com/damsleth/owa-tools/archive/refs/tags/v0.2.1.tar.gz"
-  version "0.2.1"
-  sha256 "db94752600777c271337308ac9bb41818d55779559c05a7d93b200b1515045da"
+  url "https://github.com/damsleth/owa-tools/archive/refs/tags/v0.5.0.tar.gz"
+  version "0.5.0"
+  sha256 "ca99ea7cc71b99c51733b5ef41ca08341240fd29669bba464d424b72607d1347"
   license "MIT"
   head "https://github.com/damsleth/owa-tools.git", branch: "main"
 
@@ -17,10 +17,11 @@ class OwaTools < Formula
   end
 
   test do
-    %w[owa owa-cal owa-mail owa-graph owa-doctor owa-people owa-sched owa-drive].each do |bin_name|
+    # All nine binaries land on PATH and report the same suite version.
+    %w[owa owa-cal owa-mail owa-graph owa-doctor owa-people owa-sched owa-drive
+       owa-todo].each do |bin_name|
       assert_match version.to_s, shell_output("#{bin}/#{bin_name} --version")
     end
     system "#{bin}/owa", "list"
-    system "#{bin}/owa", "doctor", "--no-tokens"
   end
 end

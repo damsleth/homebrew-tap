f03aac4d7ce12d3743b116d43b3ab30434cb7c52
Carl Joakim Damsleth <kim@damsleth.no>
2026-06-18 23:30:14 +0200

    owa-tools 1.0.0

--- Formula/owa-tools.rb
+++ Formula/owa-tools.rb
@@ -3,9 +3,9 @@ class OwaTools < Formula
 
   desc "Outlook/Microsoft 365 CLI suite (mail, calendar, graph, drive, todo, video)"
   homepage "https://github.com/damsleth/owa-tools"
-  url "https://github.com/damsleth/owa-tools/archive/refs/tags/v0.11.1.tar.gz"
-  version "0.11.1"
-  sha256 "000847bf0ca4f7f94a72c644ad765d1701422789c09a3270dad5c9f2e5ebd699"
+  url "https://github.com/damsleth/owa-tools/archive/refs/tags/v1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "1cbb703698397345b05b0f4da4532dcf2c49f12440fdd530ba0efaac99fbc7e5"
   license "MIT"
   head "https://github.com/damsleth/owa-tools.git", branch: "main"
 
